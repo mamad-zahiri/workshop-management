e3a4d518462aa355bf0cbcd29778440489416021
Mohamad Zahiri <mamadzhri@gamil.com>
2024-07-14 16:52:58 +0330

    feat(utils): add `TimeStampedModel` util class

--- src/utils/models.py
+++ src/utils/models.py
@@ -11,3 +11,18 @@ class UuidModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class TimeStampedModel(models.Model):
+    """An abstract model that contains timestamp information.
+
+    Since timestamp fields (created_at, updated_at) are common among almost all
+    models, this will help with reducing duplicate code.
+    """
+
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    modified_at = models.DateTimeField(_("Modified at"), auto_now=True)
+
+    class Meta:
+        abstract = True
+"""  """
